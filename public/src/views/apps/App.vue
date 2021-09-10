c0f70dc880ee25b0abda1fe18cc57b0888f2c6f4
MullerWlad <57362446+MullerWlad@users.noreply.github.com>
2021-09-10 23:38:11 +0300

    Add files via upload

---
+++ public/src/views/apps/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    .centred.dev-panel.display-horison-around
+</template>
+
+<script>
+    import Htw from '../components/Price.vue'
+    export default {
+        components: {
+            Htw
+        },
+        data(){
+            return {
+                data: {}
+            }
+        },
+        mounted(){}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ public/src/views/components/Price.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    div
+        include ../../pug/components/house-to-watch
+</template>
+
+<script>
+    export default {
+        props: ['data']
+    }
+</script>
+
+<style scoped>
+
+</style>
